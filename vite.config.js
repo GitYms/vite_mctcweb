5ccd7cdc0a2c5141fc58342119caab469d522b1e
Booty <mi_msyuan@outlook.com>
2021-06-26 17:03:40 +0800

    feat:添加axios请求、antd按需加载、

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import vitePluginImp from 'vite-plugin-imp'
+import path from 'path'
+import fs from 'fs'
+import lessToJS from 'less-vars-to-js'
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './config/variables.less'), 'utf8')
+)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    vitePluginImp({
+      libList: [{
+        libName: 'antd',
+        style: (name) => `antd/lib/${name}/style/index.less`,
+      },],
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables,
+      }
+    }
+  },
+  server: {
+    port: 3001, // 开发环境启动的端口
+    proxy: {
+      '/api': {
+        // 当遇到 /api 路径时，将其转换成 target 的值
+        // target: 'http://47.99.134.126:28019/api/v1',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 将 /api 重写为空
+      }
+    }
+  }
 })
