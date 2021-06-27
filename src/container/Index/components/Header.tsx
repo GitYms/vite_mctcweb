9353d3f32b8ac956e2b3e763f183abaefcc56b7c
Booty <mi_msyuan@outlook.com>
2021-06-27 21:36:56 +0800

    feat:add routers

--- src/container/Index/components/Header.tsx
+++ src/container/Index/components/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <div>header</div>
+    <div>
+      header
+      <Link to="/home">link to home</Link>
+    </div>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -12,11 +12,63 @@ import Home from '../container/Home'
 const routes = [
   {
     path: "/home",
+    name: '首页',
     component: Home,
   },
   {
     path: "/about",
+    name: '关于我们',
     component: About,
+  },
+  {
+    path: "/news",
+    name: '人财资讯',
+    component: About,
+    routes: [
+      {
+        path: '/news/deep',
+        name: '深度视野',
+      },{
+        path: '/news/policy',
+        name: '政策导向',
+      }, {
+        path: '/news/talent',
+        name: '天府菁才'
+      }
+    ]
+  }, {
+    path: '/vip',
+    name: '会员之窗',
+    component: About,
+    routes: [
+      {
+        path: '/vip/ceo',
+        name: 'CEO会客厅',
+      }, {
+        path: '/vip/moment',
+        name: '精彩瞬间'
+      }
+    ]
+  }, {
+    path: '/cooperation',
+    name: '商务合作',
+    component: About,
+  }, {
+    path: '/joinus',
+    name: '加入我们',
+    component: About,
+    routes: [
+      {
+        path: '/joinus/rule',
+        name: '入会章程',
+      },{
+        path: '/joinus/apply',
+        name: '入会申请',
+      }, {
+        path: '/joinus/bonus',
+        name: '会员福利'
+      }
+    ]
   }
 ];
 
